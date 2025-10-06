ecbae4b26faab06c6a463481660dec0daa37386e
sai krishna <sai.krishna@ivymobitech.com>
2025-10-06 17:03:06 +0530

    Add admin-service files to microservices project

---
+++ admin-service/src/test/java/com/microservice/adminservice/admin_service/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.adminservice.admin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
